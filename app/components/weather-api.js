399bfa483db8ae6e492210a2322a1af6701db05f
Kyle Lange and Martin Cartledge <martin@>
2016-11-07 11:57:38 -0800

    add weather-api service and component. create one-to-many reationship between location and event

---
+++ app/components/weather-api.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+
+export default Ember.Component.extend({
+  weatherApi: Ember.inject.service(),
+
+  actions: {
+    getWeather() {
+
+    }
+  }
+});
--- app/models/location.js
+++ app/models/location.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  image: DS.attr('image'),
+  name: DS.attr('name'),
+  directions: DS.attr('directions'),
+  lat: DS.attr('lat'),
+  long: DS.attr('long'),
+  location-pub: DS.attr('location-pub'),
+  rating: DS.attr('rating'),
+  description: DS.attr('description'),
+  events: DS.hasMany('event', { async: true })
 });
