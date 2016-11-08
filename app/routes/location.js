0afb4c39367549d9fd0fce9c721a6f9286a59f2d
martin cartledge <martincartledge@icloud.com>
2016-11-08 14:32:29 -0800

    remove content from location route

--- app/routes/location.js
+++ app/routes/location.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-//   model: function(params) {
-//     var key = config.myApiKey;
-//     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + params.city + '&appid=' + '6093f3b9eaaf68a2e9230a56390afe3e';
-//     return Ember.$getJSON(url).then(function(responseJSON) {
-//       return "responseJSON.results";
-//     });
-//   }
+
 });
