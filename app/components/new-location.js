d10e49d50c14f9c24b8f862c9bfdffee80c5cbef
Kyle Lange <baronsintrees@gmail.com>
2016-11-08 14:41:04 -0800

    add a functional add location form with working boolean checkbox for public/private location.

--- app/components/new-location.js
+++ app/components/new-location.js
@@ -5,6 +5,19 @@ export default Ember.Component.extend({
   actions: {
     locationFormShow() {
       this.set('addLocation', true);
+    },
+    saveLocation(params) {
+      var params = {
+        image: this.get('image'),
+        name: this.get('name'),
+        directions: this.get('directions'),
+        description: this.get('description'),
+        rating: 0,
+        locationPub: this.get('locationPub'),
+      }
+    this.set('addLocation', false);
+    //console.log(this.get('locationPub'));
+    this.sendAction('saveLocation', params);
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('location');
   },
+  actions: {
+    saveLocation(params) {
+      var newLocation = this.store.createRecord('location', params);
+      newLocation.save();
+      this.transitionTo('index');
+    }
+  }
 });
